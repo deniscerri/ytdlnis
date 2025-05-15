1454e75a9b437cd4a6059c9b9fbaad7075f619da
Max Jakobitsch <maxja@posteo.net>
2025-05-15 18:22:40 +0200

    fix: disable cut for live videos
    on tap show snackbar "cutting live videos is unsupported"

--- app/src/main/java/com/deniscerri/ytdl/ui/downloadcard/DownloadAudioFragment.kt
+++ app/src/main/java/com/deniscerri/ytdl/ui/downloadcard/DownloadAudioFragment.kt
@@ -337,6 +337,9 @@ class DownloadAudioFragment(private var resultItem: ResultItem? = null, private 
                                 if(isUpdatingData){
                                     val snack = Snackbar.make(view, context.getString(R.string.please_wait), Snackbar.LENGTH_SHORT)
                                     snack.show()
+                                }else if (downloadItem.duration=="0:00"){
+                                    val snack = Snackbar.make(view, context.getString(R.string.cut_live_unsupported), Snackbar.LENGTH_SHORT)
+                                    snack.show()
                                 }else if (!nonSpecific){
                                     val snack = Snackbar.make(view, context.getString(R.string.cut_unavailable), Snackbar.LENGTH_SHORT)
                                     snack.setAction(R.string.update){
