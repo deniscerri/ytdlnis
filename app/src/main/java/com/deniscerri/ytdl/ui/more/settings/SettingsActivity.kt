5329b5da96816438bd5afd560c3179e0065c64af
John Cristian Lucas <jhaiianbetter@gmail.com>
2026-02-15 09:55:59 +0800

    Implement search functionality in SettingsActivity
    Added search functionality to filter settings based on user input.

--- app/src/main/java/com/deniscerri/ytdl/ui/more/settings/SettingsActivity.kt
+++ app/src/main/java/com/deniscerri/ytdl/ui/more/settings/SettingsActivity.kt
@@ -2,6 +2,8 @@ package com.deniscerri.ytdl.ui.more.settings
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.activity.addCallback
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -9,11 +11,14 @@ import androidx.navigation.fragment.findNavController
 import com.deniscerri.ytdl.R
 import com.deniscerri.ytdl.databinding.ActivitySettingsBinding
 import com.deniscerri.ytdl.ui.BaseActivity
+import com.google.android.material.textfield.TextInputEditText
 
 
 class SettingsActivity : BaseActivity() {
     var context: Context? = null
     lateinit var binding: ActivitySettingsBinding
+    private lateinit var searchEditText: TextInputEditText
+    
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         context = baseContext
@@ -22,6 +27,9 @@ class SettingsActivity : BaseActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.frame_layout) as NavHostFragment
         val navController = navHostFragment.findNavController()
+
+        searchEditText = binding.settingsSearchEditText
+        setupSearchFunctionality()
 
         val listener = NavController.OnDestinationChangedListener { controller, destination, arguments ->
             if (destination.id == R.id.mainSettingsFragment){
@@ -44,6 +52,28 @@ class SettingsActivity : BaseActivity() {
         }
 
         if (savedInstanceState == null) navController.navigate(R.id.mainSettingsFragment)
+    }
+    
+    private fun setupSearchFunctionality() {
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            
+            override fun afterTextChanged(s: Editable?) {
+                val query = s?.toString() ?: ""
+                filterSettings(query)
+            }
+        })
+    }
+    
+    private fun filterSettings(query: String) {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.frame_layout) as? NavHostFragment
+        val currentFragment = navHostFragment?.childFragmentManager?.fragments?.firstOrNull()
+        
+        if (currentFragment is BaseSettingsFragment) {
+            currentFragment.filterPreferences(query)
+        }
     }
 
     fun changeTopAppbarTitle(text: String) {
