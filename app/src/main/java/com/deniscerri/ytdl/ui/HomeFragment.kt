fcf6b53eaeddf48fe77fd15476308fb46ebb79d9
Nexfu22 <ym2222007m@gmail.com>
2026-09-14 17:23:06 +0300

    Hide home search bar

--- app/src/main/java/com/deniscerri/ytdl/ui/HomeFragment.kt
+++ app/src/main/java/com/deniscerri/ytdl/ui/HomeFragment.kt
@@ -167,7 +167,6 @@ class HomeFragment : Fragment(), HomeAdapter.OnItemClickListener, SearchSuggesti
         // ==========================================
         searchBar?.visibility = View.GONE
         searchBar?.isEnabled = false
-        // ==========================================
         appBarLayout = view.findViewById(R.id.home_appbarlayout)
         materialToolbar = view.findViewById(R.id.home_toolbar)
         queriesChipGroup = view.findViewById(R.id.queries)
